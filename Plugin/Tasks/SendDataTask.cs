229e2c882b0f53b40550f917e292cc2425f07f2c
Mikhaël Regni <mikhael@uping.be>
2018-04-20 13:57:56 +0200

    build fix

--- Plugin/Tasks/SendDataTask.cs
+++ Plugin/Tasks/SendDataTask.cs
@@ -9,14 +9,20 @@ namespace Plugin.Tasks
 {
     public class SendDataTask : IScheduledTask
     {
-        public Task Execute(CancellationToken cancellationToken, IProgress<double> progress)
+        public async Task Execute(CancellationToken cancellationToken, IProgress<double> progress)
         {
-            
+            await Task.FromResult(true);
         }
 
         public IEnumerable<TaskTriggerInfo> GetDefaultTriggers()
         {
-            
+            return new[]
+            {
+                new TaskTriggerInfo
+                {
+                    Type = TaskTriggerInfo.TriggerDaily
+                }
+            };
         }
 
         public string Name => "Send extra data";
